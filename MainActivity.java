ad1f8ef9070472ad818526a4641823402e83958d
deoxys0325 <f3256f3256@gmail.com>
2017-12-25 01:01:22 +0800

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
+    private Button button1, button2, button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,16 +16,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //取得此Button的實體
-        button = (Button) findViewById(R.id.button1);
-
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
         //實做OnClickListener界面
-        button.setOnClickListener(new View.OnClickListener() {
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //初始化Intent物件
                 Intent intent = new Intent();
                 //從MainActivity 到Main2Activity
                 intent.setClass(MainActivity.this, MapsActivity.class);
+                //開啟Activity
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //初始化Intent物件
+                Intent intent = new Intent();
+                //從MainActivity 到Main2Activity
+                intent.setClass(MainActivity.this, PublicFacility.class);
+                //開啟Activity
+                startActivity(intent);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //初始化Intent物件
+                Intent intent = new Intent();
+                //從MainActivity 到Main2Activity
+                intent.setClass(MainActivity.this, EventInfo.class);
                 //開啟Activity
                 startActivity(intent);
             }
